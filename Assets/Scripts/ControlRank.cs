9c0c9003218f8fdb051b40178c880833919a3679
dilowagner <diegowagner4@gmail.com>
2018-01-14 19:12:52 -0200

    Update ControlRank script

--- Assets/Scripts/ControlRank.cs
+++ Assets/Scripts/ControlRank.cs
@@ -12,7 +12,7 @@ public class ControlRank : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
-		Debug.Log ("Pontuacao: " + rank.ToString());
-		//points.text = rank.ToString ();	
+		//Debug.Log ("Pontuacao: " + rank.ToString());
+		points.text = rank.ToString ();	
 	}
 }
