a4895afe4ae2af836a61f8c25a7b1626dbddf5e4
dilowagner <diegowagner4@gmail.com>
2018-01-14 18:53:28 -0200

    Update - sending particles and scores - bow

---
+++ Assets/Scripts/ControlRank.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControlRank : MonoBehaviour 
+{
+	// Contador de Rank
+	public int rank = 0;
+	public Text points;
+		
+	// Update is called once per frame
+	void Update () 
+	{
+		Debug.Log ("Pontuacao: " + rank.ToString());
+		//points.text = rank.ToString ();	
+	}
+}
--- Assets/Scripts/TargetLevel.cs
+++ Assets/Scripts/TargetLevel.cs
@@ -13,18 +13,22 @@ public class TargetLevel : MonoBehaviour
 	void OnCollisionEnter(Collision collision)
 	{
 		transform.parent.GetComponent<Target> ().particleDust.Play ();
+		ControlRank control = FindObjectOfType<ControlRank> ();
 
 		if (target == Target.TargetEnum.Level1) 
 		{
 			transform.parent.GetComponent<Target> ().particle100Points.Play ();
+			control.rank += valLevel1;
 		} 
 		else if (target == Target.TargetEnum.Level2) 
 		{
 			transform.parent.GetComponent<Target> ().particle50Points.Play ();
+			control.rank += valLevel2;
 		} 
 		else if (target == Target.TargetEnum.Level3) 
 		{
 			transform.parent.GetComponent<Target> ().particle10Points.Play ();
+			control.rank += valLevel3;
 		}
 	}
 }
