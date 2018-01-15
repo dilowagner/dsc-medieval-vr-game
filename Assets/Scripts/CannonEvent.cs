a024f3030ceeede35e3e982e380ac954d79d287d
dilowagner <diegowagner4@gmail.com>
2018-01-15 21:53:25 -0200

    Sending CannonEvent script

---
+++ Assets/Scripts/CannonEvent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CannonEvent : EventTrigger
+{
+	public override void OnPointerClick(PointerEventData eventData)
+	{
+		base.OnPointerClick (eventData);
+	}
+
+	public override void OnPointerDown(PointerEventData eventData)
+	{
+		base.OnPointerDown (eventData);
+
+		// Desativa os componentes do Player
+		FindObjectOfType<Bow>().gameObject.SetActive(false);
+	}
+}
